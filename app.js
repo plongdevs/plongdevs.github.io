ddf13cbe68a250c968029874753d608123b55b57
plongdev <phamlongsoftware@gmail.com>
2025-11-02 17:03:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const apps = [
         name: 'PlayPing Premium v1.6.4 ✅',
         url: 'https://link4m.com/kRzzJa',
         img: './img/appicons/fạkelag playping.jpg',
-        descriptions: 'PlayPing FakeLag Premium by PLongXNguyenTie',
-        size: '352 KB',
+        descriptions: 'PlayPing FakeLag Premium by PLongXNguyễnTiêu',
+        size: '7.5 MB',
         updated: '2/11/2025'
     },
     {
